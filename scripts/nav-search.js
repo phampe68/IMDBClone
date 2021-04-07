b8607c5c665202af927a72c785ff5462dd092a35
phampe68 <38918965+phampe68@users.noreply.github.com>
2021-04-07 15:00:39 -0400

    quick bugfix for advanced search edge case

--- scripts/nav-search.js
+++ scripts/nav-search.js
@@ -1,5 +1,3 @@
-// credit: https://stackoverflow.com/questions/8029532/how-to-prevent-submitting-the-html-forms-input-field-value-if-it-empty
-
 
 /**
  * handler for advanced search on submit, removes inputs from url if no value is entered in that input, prevents form submission if all inputs are missing values
@@ -9,12 +7,14 @@ const submitAdvancedSearch = () => {
     let form = document.getElementById('advanced-search-form');
     let somethingEntered = false;
 
+    let badInputs = [];
+
     //find out if something is entered, remove name property from input if there's nothing entered (this removes it from the url)
     for(let input of form.childNodes){
         if(input.value !== "")
             somethingEntered = true;
         else
-            input.name = "";
+            badInputs.push(input);
     }
 
     //alert user if nothing entered
@@ -22,5 +22,10 @@ const submitAdvancedSearch = () => {
         alert("Please enter a value in one of the textboxes.");
         return false;
     }
+
+    //remove names of bad inputs to exclude from url
+    badInputs.forEach(input => {
+        input.name = "";
+    })
     return true;
 }
