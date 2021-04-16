89285b229777879f4c91c10a0e6e7e860c7182c2
phampe68 <38918965+phampe68@users.noreply.github.com>
2021-04-16 18:42:47 -0400

    pug changes

--- database/data-models/movie-model.js
+++ database/data-models/movie-model.js
@@ -2,20 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let movieSchema = Schema({
     title: {type: String, required: true},
-    year: {type: String},
-    averageRating: {type: Number},
-    rated: {type: String},
-    released: {type: String},
-    runtime: {type: String},
-    genre: [String],
+    year: {type: String, required: true},
+    rated: {type: String, required: true},
+    released: {type: String, required: true},
+    runtime: {type: String, required: true},
+    genre: {type: [String], required: true},
     director: [{type: Schema.Types.ObjectId, ref: 'Person'}],
     actor: [{type: Schema.Types.ObjectId, ref: 'Person'}],
     writer: [{type: Schema.Types.ObjectId, ref: 'Person'}],
-    plot: {type: String},
+    plot: {type: String, required: true},
     awards: [{type: String}],
     poster: {type: String},
     reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}],
-    relatedMovies: [{type: Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
 let Movie = mongoose.model("Movie", movieSchema);
