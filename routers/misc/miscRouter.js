31183c054025c2fced1e1272f6e5bc5e7311578b
phampe68 <38918965+phampe68@users.noreply.github.com>
2021-09-02 00:30:00 -0400

    add home page suggestions

--- routers/misc/miscRouter.js
+++ routers/misc/miscRouter.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const pug = require('pug');
 
 const User = require("../../database/data-models/user-model");
+const Movie = require('../../database/data-models/movie-model.js');
 const checkLogin = require('../users/checkLogin');
+const getRecommendedMovies = require("../users/getRecommendedMovies");
 
 let router = express.Router();
 router.use(express.urlencoded({extended: true}));
@@ -27,15 +29,25 @@ const loadContribute = (req, res, next) => {
 }
 
 // loads default page
-const loadHome = (req, res, next) => {
-    let data = pug.renderFile('./templates/screens/index.pug');
+const loadHome = async (req, res, next) => {
+    const maxMovies = 6; // how many movies to display on the front page
+
+    // get recommended movies
+    let user = await User.findById(req.session.userId);
+    let recommendedMovieIDs = [];
+    await getRecommendedMovies(user, req.session.viewedMovies).then(movieIDs => {recommendedMovieIDs = movieIDs;})
+    let recommendedMovies = await Movie.find({'_id': {$in: user.moviesWatched}}).limit(5);
+
+    let randomMovies = await Movie.find().limit(maxMovies - recommendedMovies.length);
+    recommendedMovies = recommendedMovies.concat(randomMovies);
+
+    let data = pug.renderFile('./templates/screens/index.pug', {recommendedMovies});
     res.status(200).send(data);
 }
 
 // redirects to logged in users page
 const loadLoggedInUser = (req, res, next) => {
     res.status(200).redirect(`/users/${req.session.userId}/`);
-
 }
 router.get("/", [checkLogin, loadHome]);
 router.get("/contribute", [checkLogin, loadContribute]);
